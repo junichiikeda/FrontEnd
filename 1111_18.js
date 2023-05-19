c507a87140797dfa30182994d4953cec57da9b2e
junichiikeda <126534984+junichiikeda@users.noreply.github.com>
2023-05-19 16:30:26 +0900

    first commit

---
+++ 1111_18.js
@@ -0,0 +1,11 @@
+// 以下に実装してください
+
+/* <div id=”p2”></div>に「テキスト文字列」というテキスト文字列を埋め込む */
+let e2 = document.getElementById('p2');
+e2.innerText = 'テキスト文字列';
+
+
+// <div id=”p3”></div>のid属性の値を”p5”に変更する
+let e3 = document.getElementById('p3');
+e3.id = 'p5';
+console.log(e3.id);
